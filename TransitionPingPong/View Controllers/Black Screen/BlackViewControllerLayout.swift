e7e49c6c98577b89ab788f893879cf17a789f026
IJke <ijke@overscope.net>
2017-12-19 22:06:55 +0100

    Remove unnecessary stuff, add white and black view controllers, add white and black view controller layout files

---
+++ TransitionPingPong/View Controllers/Black Screen/BlackViewControllerLayout.swift
@@ -0,0 +1,9 @@
+//
+//  BlackViewControllerLayout.swift
+//  TransitionPingPong
+//
+//  Created by IJke Botman on 19/12/2017.
+//  Copyright © 2017 IJke Botman. All rights reserved.
+//
+
+import Foundation
