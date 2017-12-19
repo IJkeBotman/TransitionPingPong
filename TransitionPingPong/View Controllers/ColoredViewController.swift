c511cf753fce7768ff8809fcdd8b62fc0a01390a
IJke <ijke@overscope.net>
2017-12-19 22:14:32 +0100

    Create ColorViewController

--- TransitionPingPong/View Controllers/ColoredViewController.swift
+++ TransitionPingPong/View Controllers/ColoredViewController.swift
@@ -6,4 +6,56 @@
 //  Copyright © 2017 IJke Botman. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum Color {
+    case white
+    case black
+}
+
+class ColoredViewController: UIViewController {
+    let triggerButton = UIButton()
+    let contentTextView = UITextView()
+    
+    let color: Color
+    
+    init(color: Color) {
+        self.color = color
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = color(for: color)
+        contentTextView.isUserInteractionEnabled = false
+        
+        triggerButton.addTarget(self, action: #selector(buttonWasTapped), for: .touchUpInside)
+        
+        self.view.addSubview(contentTextView)
+        self.view.addSubview(triggerButton)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        view.setNeedsLayout()
+    }
+    
+    func color(for color: Color) -> UIColor {
+        switch color {
+        case .white:
+            return .white
+        case .black:
+            return .black
+        }
+    }
+    
+    @objc func buttonWasTapped() {
+        assertionFailure("Should be implement in subclasses.")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Init Coder not implemented.")
+    }
+}
